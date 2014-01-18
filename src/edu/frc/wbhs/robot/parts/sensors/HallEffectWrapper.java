4bd9320f674ec657e1ec07ce9e0ac167ad244842
Josh Trate <jtscuba@gmail.com>
2014-01-17 21:28:15 -0500

    makes hall effect sensor analog

--- src/edu/frc/wbhs/robot/parts/sensors/HallEffectWrapper.java
+++ src/edu/frc/wbhs/robot/parts/sensors/HallEffectWrapper.java
@@ -7,6 +7,8 @@
 package edu.frc.wbhs.robot.parts.sensors;
 
 import edu.wpi.first.wpilibj.Counter;
+import edu.wpi.first.wpilibj.AnalogTrigger;
+import edu.wpi.first.wpilibj.AnalogChannel;
 
 /**
  *
@@ -19,8 +21,11 @@ public class HallEffectWrapper {
         private double pi = 3.141592673;
         private double wheelDiameter = 8.0;
 	
-	public HallEffectWrapper(int pinID) {
-		counter = new Counter(pinID);
+	public HallEffectWrapper(int pinID) 
+        {
+            AnalogTrigger trigger = new AnalogTrigger(pinID);
+            counter = new Counter(trigger);
+            
 	}
 	
 	public double getLastPeriod() {
