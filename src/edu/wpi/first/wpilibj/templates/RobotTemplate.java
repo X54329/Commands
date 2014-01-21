9299e1d001b20b6b57dc059c4a6982344cec3a05
X54329 <bobojoe108@gmail.com>
2014-01-21 16:10:02 -0500

    Begin work on dashboard

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -6,46 +6,48 @@
 /*----------------------------------------------------------------------------*/
 package edu.wpi.first.wpilibj.templates;
 
+import edu.frc.wbhs.dashboard.Dashboard;
 import edu.frc.wbhs.robot.Robot;
 import edu.frc.wbhs.robot.parts.chassis.Chassis;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
-import edu.wpi.first.wpilibj.networktables.NetworkTableProvider;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class RobotTemplate extends IterativeRobot {
 
 	public static double PARABOLA_VELOCITY_CONSTANT = 0;
-	public int JOYSTICK = 1; // which joystick we are using, usually 1
-	public static final int X_AXIS_CHANNEL = 1; // which channel the x axis is on the joystick
-	public static final int Y_AXIS_CHANNEL = 2;
-	public static final int Z_AXIS_CHANNEL = 3;
-	public static final int[] RIGHT_SIDE_PINS = new int[]{1}; //which digital output the right side of the drive motors are
-	public static final int[] LEFT_SIDE_PINS = new int[]{2};
-	public static final int GYRO_PIN = 1; // analog input
-	public static final int ACCELEROMETER_PIN = 2; // analog input
-	public static final double ROBOT_MAX_ANGULAR_SPEED = 250; //in degrees per second
-	public static final double GYRO_PID_P = 0;
-	public static final double GYRO_PID_I = 0;
-	public static final double GYRO_PID_D = 0;
-	public static final double GYRO_PID_F = 0;
-	public static final double GYRO_PID_MULTIPLIER = 0.9;
-	public static final int POTID = 0;
-	public static final double MOTOR_TO_VELOCITY_PROPORTION = 0;
-	public static final double G = 9.8;
-	public static final double THETA = 0; // Angle of shooting
+	public static int JOYSTICK = 1; // which joystick we are using, usually 1
+	public static int X_AXIS_CHANNEL = 1; // which channel the x axis is on the joystick
+	public static int Y_AXIS_CHANNEL = 2;
+	public static int Z_AXIS_CHANNEL = 3;
+	public static int[] RIGHT_SIDE_PINS = new int[]{1}; //which digital output the right side of the drive motors are
+	public static int[] LEFT_SIDE_PINS = new int[]{2};
+	public static int GYRO_PIN = 1; // analog input
+	public static int ACCELEROMETER_PIN = 2; // analog input
+	public static double ROBOT_MAX_ANGULAR_SPEED = 250; //in degrees per second
+	public static double GYRO_PID_P = 0;
+	public static double GYRO_PID_I = 0;
+	public static double GYRO_PID_D = 0;
+	public static double GYRO_PID_F = 0;
+	public static double GYRO_PID_MULTIPLIER = 0.9;
+	public static int POTID = 0;
+	public static double MOTOR_TO_VELOCITY_PROPORTION = 0;
+	public static double G = 9.8;
+	public static double THETA = 0; // Angle of shooting
 	public static double USD_PID_P = 0.05;
 	public static double USD_PID_I = 0;
 	public static double USD_PID_D = 0;
 	public static double USD_PID_F = 0;
 	public static int USD_PIN_IN = 7;
 	public static int USD_PIN_OUT = 8;
-	public static final double SHOOTING_DISTANCE = 15;
+	public static double SHOOTING_DISTANCE = 15;
 
 	public Robot robot;
 	public Chassis chassis;
 	public Joystick joystick;
 	public NetworkTable Output;
+	public SmartDashboard dashboard;
 
 	public void robotInit() {
 		//NetworkTable Output =  new NetworkTable("Output", new NetworkTableProvider(new NetworkTableNode()));
@@ -53,6 +55,8 @@ public class RobotTemplate extends IterativeRobot {
 		chassis = new Chassis(RIGHT_SIDE_PINS, LEFT_SIDE_PINS, GYRO_PIN, ACCELEROMETER_PIN, POTID); //set up the chassis
 		robot = new Robot(chassis); //feed it to the robot
 		joystick = new Joystick(JOYSTICK);
+		dashboard = new SmartDashboard();
+
 	}
 
 	/**
@@ -70,7 +74,8 @@ public class RobotTemplate extends IterativeRobot {
 		//USD_PID_P = Output.getNumber("P", 0.5);
 		//USD_PID_I = Output.getNumber("I", 0);
 		//USD_PID_D = Output.getNumber("D", 0);
-		
+		Dashboard.getNumbers();
+		Dashboard.putNumbers();
 		robot.shoot();
 	}
 
