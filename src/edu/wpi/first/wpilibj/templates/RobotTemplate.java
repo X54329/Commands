53f08d6b97fb6d1ed23ccfc027cf6c012664031f
jtscuba <jtscuba@gmail.com>
2014-01-22 12:13:32 -0500

    Spike Code

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -41,12 +41,9 @@ public class RobotTemplate extends IterativeRobot {
 	public static double USD_PID_F = 0;
 	public static int USD_PIN_IN = 7;
 	public static int USD_PIN_OUT = 8;
-<<<<<<< HEAD
-	public static double SHOOTING_DISTANCE = 15;
-=======
 	public static final double SHOOTING_DISTANCE = 15;
 	public static final int SpikePin = 34;
->>>>>>> origin/josh
+
 
 	public Robot robot;
 	public Chassis chassis;
