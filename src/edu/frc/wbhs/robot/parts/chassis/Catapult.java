86046f83210a89f4a39bd535d2787752aae222e8
Brian <Brian@192.168.1.2>
2014-01-23 12:21:29 -0500

    Added bit of framework for catapult and arms

---
+++ src/edu/frc/wbhs/robot/parts/chassis/Catapult.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.frc.wbhs.robot.parts.chassis;
+
+/**
+ *
+ * @author Brian
+ */
+public class Catapult {
+	
+	// Motor motor1;
+	// Motor motor2;
+	
+	// TODO: add methods for operation
+	
+}
