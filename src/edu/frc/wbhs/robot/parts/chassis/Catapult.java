122aee88bf4db3d808a9fb2a7bfe8585c7e57226
Brian <Brian@192.168.1.2>
2014-01-25 12:20:24 -0500

    working on pids

--- src/edu/frc/wbhs/robot/parts/chassis/Catapult.java
+++ src/edu/frc/wbhs/robot/parts/chassis/Catapult.java
@@ -6,7 +6,12 @@
 package edu.frc.wbhs.robot.parts.chassis;
 
 import edu.frc.wbhs.robot.parts.Motor;
+import edu.frc.wbhs.robot.parts.pid.PIDOut;
+import edu.frc.wbhs.robot.parts.pid.PIDSauce;
+import edu.frc.wbhs.robot.parts.pid.PIDWrapper;
+import edu.frc.wbhs.robot.parts.sensors.PotWrapper;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.templates.RobotTemplate;
 
 /**
  *
@@ -16,8 +21,16 @@ public class Catapult {
 
 	private Motor motor1;
 	private Motor motor2;
+	
 	private DigitalInput stopsensorFront;
 	private DigitalInput stopsensorBack;
+	
+	private PotWrapper pot;
+	private double backVolts;
+	
+	private PIDWrapper potPID;
+	private PIDOut potPIDOut;
+	private PIDSauce potPIDSauce;
 
 	public Catapult(int[] motorPinIds, int[] digiInputIds) {
 		motor1 = new Motor(motorPinIds[0]);
@@ -25,22 +38,29 @@ public class Catapult {
 		stopsensorFront = new DigitalInput(digiInputIds[0]);
 		stopsensorBack = new DigitalInput(digiInputIds[1]);
 	}
-
-	public void update() {
-		if (stopsensorFront.get() || stopsensorBack.get()) {
-			stop();
-		}
+	
+	public void calibrateBackAngle() {
+		backVolts = pot.getVoltage();
 	}
 
-	public void shoot() {
-		motor1.setPower(1.0);
-		motor2.setPower(1.0);
+	public void update() {
+		if (stopsensorFront.get()) {
+			stop();
+		}
+		/*potPIDOut = new PIDOut();
+		potPIDSauce = new PIDSauce(0);
+		potPID = new PIDWrapper(RobotTemplate.SHOOT_PID_P, RobotTemplate.SHOOT_PID_I, RobotTemplate.SHOOT_PID_D, RobotTemplate.SHOOT_PID_F, potPIDSauce, potPIDOut, 0.05);*/
+	}
+
+	public void shoot(double power) {
+		motor1.setPower(power);
+		motor2.setPower(power);
 	}
 
 	public void reset() {
 		// TODO: Add PID to make sure it stays back
-		motor1.setPower(-1.0);
-		motor2.setPower(-1.0);
+		motor1.setPower(-0.1);
+		motor2.setPower(-0.1);
 	}
 
 	public void stop() {
