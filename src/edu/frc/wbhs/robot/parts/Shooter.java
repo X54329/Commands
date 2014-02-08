fc2338107a6c825549e49ed4cc1a3a9dc2466fab
X54329 <bobojoe108@gmail.com>
2014-02-08 10:44:20 -0500

    still broke

--- src/edu/frc/wbhs/robot/parts/Shooter.java
+++ src/edu/frc/wbhs/robot/parts/Shooter.java
@@ -28,7 +28,7 @@ public class Shooter {
         private boolean readyToFire;
         
         public Shooter() {
-                USD = new USDWrapper(RobotTemplate.USD_PIN_IN, RobotTemplate.USD_PIN_OUT);
+                USD = new USDWrapper(RobotTemplate.USD_PIN_IN[0], RobotTemplate.USD_PIN_OUT[0]);
                 udsPIDOut = new PIDOut();
                 udsPIDSauce = new PIDSauce(1);
                 udsPID = new PIDWrapper(RobotTemplate.USD_PID_P, RobotTemplate.USD_PID_I, RobotTemplate.USD_PID_D, RobotTemplate.USD_PID_F, udsPIDSauce, udsPIDOut, 0.1);
