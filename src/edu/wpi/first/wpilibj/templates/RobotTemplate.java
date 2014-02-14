4b5dd0c5b442917e6c89be8e59bd8066090d03ca
smcfin7 <smcfincannon@gmail.com>
2014-02-13 20:35:25 -0500

    Catapult power is now set to 65%.  Commit made by Zach.  Why don't I just do everything for this robot, now?

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -125,11 +125,10 @@ public class RobotTemplate extends IterativeRobot {
 
 		if (joystick.getRawButton(3)) {
 			//robot.chassis.gyro.reset();
-			
 		}
-		
+
 		if (joystick.getRawButton(2)) {
-			chassis.catapult.shoot(0.25);   //make this function take two parameters?  one per motor?
+			chassis.catapult.shoot(0.60);   //make this function take two parameters?  one per motor?
 		} else {
 			chassis.catapult.stop();
 		}
