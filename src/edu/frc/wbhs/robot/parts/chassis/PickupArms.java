a8729d10325077817ae31d2b437b5f751a2f9ed7
X54329 <bobojoe108@gmail.com>
2014-02-15 10:07:37 -0500

    Made arms movement safer

--- src/edu/frc/wbhs/robot/parts/chassis/PickupArms.java
+++ src/edu/frc/wbhs/robot/parts/chassis/PickupArms.java
@@ -18,60 +18,70 @@ import edu.wpi.first.wpilibj.templates.RobotTemplate;
  * @author Brian
  */
 public class PickupArms {
-    
-    private Motor motor1;
-    private Motor motor2;
-    private PotWrapper pot;
-    private PIDOut potPIDOut;
-    private PIDSauce potPIDSauce;
-    private PIDWrapper potPID;
-    private DigitalInputWrapper ballSwitch;
-    
-    public PickupArms(int motorPinId, int rotorMotorPinId, int potPinId, int switchPinId) {
-        motor1 = new Motor(RobotTemplate.PICKUP_ARM_MOTOR);
-        pot = new PotWrapper(RobotTemplate.PICKUP_POTENTIOMETER_PIN);
-        motor2 = new Motor(RobotTemplate.PICKUP_ARM_ROTOR_MOTOR);
-        ballSwitch = new DigitalInputWrapper(RobotTemplate.BALL_SWITCH_PIN);
-        
-        potPIDOut = new PIDOut();
-        potPIDSauce = new PIDSauce(0);
-        potPID = new PIDWrapper(RobotTemplate.ARM_PID_P, RobotTemplate.ARM_PID_I, RobotTemplate.ARM_PID_D, RobotTemplate.ARM_PID_F, potPIDSauce, potPIDOut, 0.05);
-        
-    }
-    
-    public boolean moveArmsDown() {
-        // TODO: use a pid
-        double potPidChange = 0;
-        potPIDSauce.setSauceVal(pot.getVoltage());
-        potPID.setSetpoint(RobotTemplate.POT_ARMS_DOWN_VOLT);
-        potPidChange = potPIDOut.getOutput();
-        motor1.setPower(potPidChange);
-        
-        if (pot.getVoltage() > RobotTemplate.POT_ARMS_DOWN_VOLT + 0.05 && pot.getVoltage() < RobotTemplate.POT_ARMS_DOWN_VOLT - 0.05) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
-    public void moveRollers(double power) {
-        motor2.setPower(power);
-    }
-    
-    public boolean moveArmsUp() {
-        double potPidChange = 0;
-        potPIDSauce.setSauceVal(pot.getVoltage());
-        potPID.setSetpoint(RobotTemplate.POT_ARMS_UP_VOLT);
-        potPidChange = potPIDOut.getOutput();
-        motor1.setPower(potPidChange);
-        if (pot.getVoltage() > RobotTemplate.POT_ARMS_UP_VOLT + 0.05 && pot.getVoltage() < RobotTemplate.POT_ARMS_UP_VOLT - 0.05) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
-    public boolean isBallInPlace() {
-        return ballSwitch.get();
-    }
+
+	private Motor motor1;
+	private Motor motor2;
+	private PotWrapper pot;
+	private PIDOut potPIDOut;
+	private PIDSauce potPIDSauce;
+	private PIDWrapper potPID;
+	private DigitalInputWrapper ballSwitch;
+
+	public PickupArms(int motorPinId, int rotorMotorPinId, int potPinId, int switchPinId) {
+		motor1 = new Motor(RobotTemplate.PICKUP_ARM_MOTOR);
+		pot = new PotWrapper(RobotTemplate.PICKUP_POTENTIOMETER_PIN);
+		motor2 = new Motor(RobotTemplate.PICKUP_ARM_ROTOR_MOTOR);
+		ballSwitch = new DigitalInputWrapper(RobotTemplate.BALL_SWITCH_PIN);
+
+		potPIDOut = new PIDOut();
+		potPIDSauce = new PIDSauce(0);
+		potPID = new PIDWrapper(RobotTemplate.ARM_PID_P, RobotTemplate.ARM_PID_I, RobotTemplate.ARM_PID_D, RobotTemplate.ARM_PID_F, potPIDSauce, potPIDOut, 0.05);
+
+	}
+
+	public boolean moveArmsDown() {
+		// TODO: use a pid
+		double potPidChange = 0;
+		potPIDSauce.setSauceVal(pot.getVoltage());
+		potPID.setSetpoint(RobotTemplate.POT_ARMS_DOWN_VOLT);
+		potPidChange = potPIDOut.getOutput();
+		if (pot.getVoltage() > RobotTemplate.POT_ARMS_MAX_SAFE || pot.getVoltage() < RobotTemplate.POT_ARMS_MIN_SAFE) {
+			System.out.println("Something is wrong with the Arms potentiometer! Fix it!");
+		} else {
+			motor1.setPower(potPidChange);
+		}
+
+		if (pot.getVoltage() > RobotTemplate.POT_ARMS_DOWN_VOLT + 0.05 && pot.getVoltage() < RobotTemplate.POT_ARMS_DOWN_VOLT - 0.05) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void moveRollers(double power) {
+		motor2.setPower(power);
+	}
+
+	public boolean moveArmsUp() {
+		double potPidChange = 0;
+		potPIDSauce.setSauceVal(pot.getVoltage());
+		potPID.setSetpoint(RobotTemplate.POT_ARMS_UP_VOLT);
+		potPidChange = potPIDOut.getOutput();
+
+		if (pot.getVoltage() > RobotTemplate.POT_ARMS_MAX_SAFE || pot.getVoltage() < RobotTemplate.POT_ARMS_MIN_SAFE) {
+			System.out.println("Something is wrong with the Arms potentiometer! Fix it!");
+		} else {
+			motor1.setPower(potPidChange);
+		}
+
+		if (pot.getVoltage() > RobotTemplate.POT_ARMS_UP_VOLT + 0.05 && pot.getVoltage() < RobotTemplate.POT_ARMS_UP_VOLT - 0.05) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean isBallInPlace() {
+		return ballSwitch.get();
+	}
 }
